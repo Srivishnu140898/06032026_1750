10a8d9048b05fc3bf18b3479c368cd30130d4b6d
Srivishnu140898 <vishnusri981@gmail.com>
2026-03-07 12:05:52 +0530

    re push

---
+++ Secound_pod/Controllers/SecoundController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Secound_pod.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SecoundController : ControllerBase
+    {
+
+        [HttpGet("Ping")]
+        public string get_ping()
+        {
+            return "success from secound apis";
+        }
+    }
+}
